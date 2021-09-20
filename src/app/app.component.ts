9b539b58b3f8b5adef5f531e0837ce03f5281970
Alkis Markogiannakis <venmarko@hotmail.com>
2021-09-20 05:21:19 +0300

    Angular TypingChallenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {lorem} from 'faker';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Typing';
-}
+  randomText =lorem.sentence();
+   
+  enterText='';
+
+  onInput(value:string){
+   this.enterText=(value);
+  }
+
+  compare(randomLetter:string,enteredLetter:string){
+    if(!enteredLetter){
+      return 'pending';
+    }
+
+    return randomLetter === enteredLetter ?'correct':"incorrect";
+
+  }
+
+ 
+
+  onButtonClick(){
+    const letters='abcdefghijklmnopqrstuvwxyz';
+    const symbols ='!@#$%^&*()_+';
+
+    
+  }
+  
+  }
+
